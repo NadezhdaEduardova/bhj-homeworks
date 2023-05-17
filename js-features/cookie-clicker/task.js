f39cc9ed9eeebfd348bb862b906b96a64003bac1
NadezhdaEduardova <124979618+NadezhdaEduardova@users.noreply.github.com>
2023-05-17 19:49:09 +0300

    Update task.js
    Задача 1,2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,7 +1,7 @@
 const fhoto = document.getElementById("cookie");
 const clickerCounter = document.getElementById("clicker_counter");
 
-fhoto.onclick = function () {
+fhoto.onclick = function() {
   if (clickerCounter.textContent % 2 == 0) {
     fhoto.width = 300;
   } else {
