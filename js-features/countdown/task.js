26a60ef762135bdda2fd33a772619de5ff268f71
NadezhdaEduardova <124979618+NadezhdaEduardova@users.noreply.github.com>
2023-05-17 18:47:50 +0300

    Update task.js
    задача 1

---
+++ js-features/countdown/task.js
@@ -0,0 +1,17 @@
+ const timer = document.getElementById("timer");
+
+  const timerId = setInterval(() => {
+    const value = +timer.textContent;
+    if (value > 0) {
+      timer.textContent = value - 1;
+    } else {
+      alert("«Вы победили в конкурсе!»");
+      clearInterval(timerId);
+    }
+  }, 1000)
+
+      
+  
+
+          
+          
