ff8b41f348f85da90333143f8eb017fcf554b28d
NadezhdaEduardova <124979618+NadezhdaEduardova@users.noreply.github.com>
2023-05-18 12:21:07 +0300

    Update task.js
    Задачи 1+2+3 ???

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,11 +1,11 @@
-const fhoto = document.getElementById("cookie");
+const photo = document.getElementById("cookie");
 const clickerCounter = document.getElementById("clicker_counter");
 
-fhoto.onclick = function() {
+photo.onclick = function() {
   if (clickerCounter.textContent % 2 == 0) {
-    fhoto.width = 300;
+    photo.width = 300;
   } else {
-    fhoto.width = 200;
+    photo.width = 200;
   }
   clickerCounter.textContent ++;
 }
