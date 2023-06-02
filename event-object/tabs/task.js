bb0e9a3fcec3f0caddcfb28f5207210989140bf0
NadezhdaEduardova <124979618+NadezhdaEduardova@users.noreply.github.com>
2023-06-02 16:17:11 +0300

    Update task.js
    3 ????

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,8 +1,7 @@
 const tabsBtn = Array.from(document.querySelectorAll(".tab"));
 const tabsContent = Array.from(document.querySelectorAll(".tab__content"));
 
-tabsBtn.forEach((element, index) => {
-  element.addEventListener("click", (event) => {
+tabsBtn.forEach(element => element.addEventListener("click", (event) => {
     let tabIndex = tabsBtn.indexOf(element);
     
     tabsBtn.forEach(el => el.classList.remove("tab_active"));
@@ -10,6 +9,6 @@ tabsBtn.forEach((element, index) => {
     
     element.classList.add('tab_active');
     tabsContent[tabIndex].classList.add("tab__content_active");
-    evt.preventDefault();
-  })
-})
+    event.preventDefault();
+  }))
+
