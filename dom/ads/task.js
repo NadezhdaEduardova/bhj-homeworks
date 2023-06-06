517d12834ae0e30a62c85905d3789788886f45ac
NadezhdaEduardova <124979618+NadezhdaEduardova@users.noreply.github.com>
2023-06-06 11:53:06 +0300

    Update task.js
    2 ??

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -9,6 +9,7 @@ const timer = setInterval(() => {
       if (index > rotatorCases.length - 1) {
         index = -1;
       } else {
+      el[index].classList.remove("rotator__case_active");   
       el[index + 1].classList.add("rotator__case_active");
     });
     
