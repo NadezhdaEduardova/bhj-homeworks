d03fa430d28495652f683f00f6ef3f53bc9167c8
NadezhdaEduardova <124979618+NadezhdaEduardova@users.noreply.github.com>
2023-06-06 11:46:59 +0300

    Update task.js
    2?

---
+++ dom/ads/task.js
@@ -0,0 +1,17 @@
+const rotatorCases = Array.from(document.querySelectorAll(".rotator__case"));
+
+
+const timer = setInterval(() => {
+    let index = rotatorCases.findIndex(item => item.classList.contains("rotator__case_active"));
+                                       
+    rotatorCases.forEach(el => {
+      
+      if (index > rotatorCases.length - 1) {
+        index = -1;
+      } else {
+      el[index + 1].classList.add("rotator__case_active");
+    });
+    
+}, 1000)
+
+
